c2fce656fb1589abdd90933b7fb9f829370853e1
Karl Long <kai.long@hpe.com>
2018-04-10 13:28:39 +0800

    figure out the net flow

--- TFTest.py
+++ TFTest.py
@@ -2,11 +2,14 @@ import tensorflow as tf
 import numpy as np
 
 # z = np.random.randint(0, 10, size=[5])
-z = tf.constant([4, 0, 0, 0])
-x = tf.constant([0.5, 0.2, 0.3, 0, 0])
+z = tf.constant([4, 0, 0, 0])   # action sequence
+x = tf.constant([[0.5, 0.2, 0.1, 0.1, 0.1],     # action posibility for each state
+                 [0.1, 0.2, 0.3, 0.4, 0.],
+                 [0.2, 0.2, 0.2, 0.2, 0.2],
+                 [0.5, 0.2, 0.3, 0., 0.]])
 y = tf.one_hot(z, 5, dtype=tf.float32)
 aa = x * y
-log_prob = tf.reduce_sum(aa, axis=1, keep_dims=True)
+log_prob = tf.reduce_sum(aa, axis=1, keep_dims=True)        # P(a|s)
 with tf.Session()as sess:
     print(sess.run(z))
     print(sess.run(y))
